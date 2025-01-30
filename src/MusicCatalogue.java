143ff7259fbfad1681fc620f100a3dcbe4f4c5a2
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-30 15:04:34 +0300

    First coding of task1

---
+++ src/MusicCatalogue.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicCatalogue {
+    private List<MusicEntry> entries;
+
+    public MusicCatalogue() {
+        entries = new ArrayList<>();
+    }
+
+    public void addEntry(MusicEntry entry) {
+        entries.add(entry);
+    }
+
+    public List<MusicEntry> getEntries() {
+        return entries;
+    }
+}
